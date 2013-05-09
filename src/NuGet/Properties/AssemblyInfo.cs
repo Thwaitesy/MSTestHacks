96557177ec96507df00c15274ec1f392c8a5cae1
Sam Thwaites <sam@thwaitesit.com>
2013-05-09 12:19:27 +1000

    Simplified NuGet and added licence and project site etc

--- src/NuGet/Properties/AssemblyInfo.cs
+++ src/NuGet/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MSTestHacks.NuGet")]
-[assembly: AssemblyDescription("Project template made by EyeCatch (http://www.eyecatch.no/)")]
+[assembly: AssemblyTitle("NuGet")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MSTestHacks.NuGet")]
+[assembly: AssemblyProduct("NuGet")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
+[assembly: Guid("2e9aa6ed-54c0-474e-aa0c-9b02c97e81e0")]
 
 // Version information for an assembly consists of the following four values:
 //
