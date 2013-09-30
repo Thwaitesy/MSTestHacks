4a2492e16ed0c223fa169456c22ada83eb51dda2
Sam Thwaites <sam@thwaitesit.com>
2013-09-30 16:54:14 +0930

    Fixed up readme file

--- FastSelect_NSW.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" src\MSTestHacks.sln /t:rebuild
-
-"%VS110COMNTOOLS%..\IDE\mstest.exe"  /testcontainer:src\MSTestHacks.Tests\bin\Debug\MSTestHacks.Tests.dll /resultsfile:TestResult.trx
